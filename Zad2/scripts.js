18ed6bec325926a942763fde503ef7e2d9c14d73
EmelusP <pierzchalaemil@gmail.com>
2025-11-04 19:59:37 +0100

    TAsk 2 point 8 finally working

--- Zad2/scripts.js
+++ Zad2/scripts.js
@@ -54,7 +54,7 @@ window.onload = function() {
 };
 
 async function getCategoryFromGroq(title, description) {
-    const GROQ_API_KEY ="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";
+    const GROQ_API_KEY ="Klucz";
 
     const systemPrompt = `Jesteś ekspertem od kategoryzacji zadań. Twoim zadaniem jest przypisanie JEDNEGO słowa jako kategorii dla zadania na podstawie tytułu i opisu.
 
